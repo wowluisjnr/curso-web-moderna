a5280f7fbfebc11140edd888fa9249a09c6507b3
LuisPC <wowluisjnr@gmail.com>
2019-03-08 19:16:48 -0300

    SPA com webpack, falta o html-loader

---
+++ spaComWebpack/webpack.config.js
@@ -0,0 +1,52 @@
+const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
+module.exports = {
+    mode:'development',
+    entry:'./src/assets/js/navegacao.js',
+    output:{
+        filename:'app.min.js',
+        path: __dirname + '/buildWebpack/assets'
+    },
+    optimization:{
+        minimizer:[
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "app.min.css"            
+        })
+    ],
+    module:{
+        rules:[{
+            test:/\.s?[ac]ss$/,
+            use:[
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
+        },{
+            test:/\.(png|svg|jpg|gif)$/,
+            use:['file-loader']
+        },{
+            test:/\.html$/,
+            use:[{
+                loader:'html-loader'
+
+            }]
+        }]
+    }
+}
+
+
+
+// {
+//     test: /\.html$/,
+//     use: [ {
+//       loader: 'html-loader',
+//       options: {
+//         minimize: true
+//       }
+//     }
